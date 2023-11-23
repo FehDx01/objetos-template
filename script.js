e7e1d71cf6e8cf2b43a2f49ac136555e0ee82a85
FehDx01 <lessa.fernando01@gmail.com>
2023-11-22 21:35:27 -0300

    template durante a aula

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function imprimirObjeto () {
-    //digite a sua solução aqui
+/* function imprimirObjeto () {
+    
+    const estudante = {
+        nome: "Fernando",
+        sobrenome: "Lessa",
+        numeroDaMatricula: 6722,
+        notasDoSemestre: [8, 7]
+    }
+    estudante.modulo = "Fundamentos"
+    
+    
+    const copiaEstudante = {
+        ...estudante,
+        nome: "Astrodev",
+        notasDoSemestre: [...estudante.notasDoSemestre, 9],
+        modulo: "Frontend"
+    }
+  
+        
+    const estudantesLabenu = []
+    estudantesLabenu.push(estudante, copiaEstudante)
+
+    console.log(estudantesLabenu);
+
 }
-imprimirObjeto()
+
+
+imprimirObjeto() */
